f8f1b1e3113ea6b549ea1def276c8ae4f92d444b
dem0saic <vowusu032@st.ug.edu.gh>
2024-06-23 23:46:05 +0000

    All student records added successfully in admin panel

--- Student_Records/Records/models.py
+++ Student_Records/Records/models.py
@@ -7,10 +7,10 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     student_age = models.PositiveIntegerField()
     email = models.EmailField((""), max_length=254)
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.PositiveBigIntegerField(max_length=15)
     location = models.CharField(max_length=100)
     hobby = models.CharField(max_length=100)
 
     def __str__(self):
         return f'Student: {self.first_name}, {self.last_name}'
-    
+                
